47f475637041c4740397b079b85f0a84325c1fa9
kazhem4ik <zkaifecc@gmail.com>
2026-01-05 18:27:36 +0300

    day02 full

---
+++ day02/day02.py
@@ -0,0 +1,14 @@
+name = "Nikita"
+age = 31
+height = 182
+is_student = "not student"
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+print("My name is " + name + ".")
+print("Mne " + str(age) + " age.")
+print("My height " + str(height) + "cm." )
+print("I'm "+ is_student + ".")
