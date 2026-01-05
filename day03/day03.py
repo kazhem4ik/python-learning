5bf17e50dc698dfec77c2e4c99a8edb9b63a2908
kazhem4ik <zkaifecc@gmail.com>
2026-01-05 22:15:52 +0300

    Day 03 + practic

--- day03/day03.py
+++ day03/day03.py
@@ -1,3 +1,4 @@
-age = input("Введите ваш возраст: ")
-agefive = int(age) + 5
-print("Через 5 лет вам будет: " + str(agefive) + " лет") 
+age = int(input("Введите ваш возраст: "))
+#agefive = int(age) + 5
+#print("Через 5 лет вам будет: " + str(agefive) + " лет")
+print(f"Через 5 лет вам будет: {age + 5}")
---
+++ day03/pr2.py
@@ -0,0 +1,10 @@
+number1 = int(input("Введите первое число: "))
+number2 = int(input("Введите второе число: "))
+
+summ = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print(f'''Сумма: {summ}
+Разница: {difference}
+Произведение: {product}''')
